4615ec93a17271ab6caf0ecd163ebc63f63867fa
cjbrock <corinna@flatironschool.com>
2021-02-23 11:47:03 -0500

    commit tree 16d66c36d6844f42f9b3169bf80c08c50317375d

---
+++ lib/introduction.rb
@@ -0,0 +1,11 @@
+require 'pry'
+def introduction(name)
+    puts "Hi, my name is #{name}."
+end
+
+
+
+def introduction_with_language(name, language)
+    binding.pry
+    puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
